912acd6e6ccf2f62d73c1314c894567c6e8cff56
techno2mahi <techno2mahi@gmail.com>
2019-11-25 19:40:50 +0530

    Update usp_ProcedureOne.proc.sql

--- DBScriptGen/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/usp_ProcedureOne.proc.sql
+++ DBScriptGen/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/usp_ProcedureOne.proc.sql
@@ -10,7 +10,7 @@ BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
-
+   Declare @empid int
     -- Insert statements for procedure here
 	select * from Table1
 END
