1e293b97be0a456ef641554fe94e87af28b970ce
Sultkon <sultkon@gmail.com>
2019-11-26 08:39:30 +0530

    trigger added

--- src/Schema Objects/Schemas/dbo/Tables/Table1.table.sql
+++ src/Schema Objects/Schemas/dbo/Tables/Table1.table.sql
@@ -5,37 +5,3 @@
 );
 
 
-
-
-GO
-CREATE TRIGGER dbo.trg_Table1_audit
-ON dbo.Table1
-AFTER INSERT, DELETE
-AS
-BEGIN
-    SET NOCOUNT ON;
-    INSERT INTO [dbo].[Table1_Transaction](
-        [Column1], 
-        [Column2],
-        [Column3],
-		[CreatedDate],
-		[Action] 
-    )
-    SELECT
-         [Column1], 
-        [Column2],
-        [Column3], 
-        GETDATE(),
-        'INS'
-    FROM
-        inserted i
-    UNION ALL
-    SELECT
-      [Column1], 
-        [Column2],
-        [Column3], 
-        GETDATE(),
-        'DEL'
-    FROM
-        deleted d;
-END
