8f92df13cfa7f74cd9b466697eb17d777b338ba0
Sultkon <sultkon@gmail.com>
2019-11-26 20:54:11 +0530

    Added table Release Version

---
+++ src/Schema Objects/Schemas/dbo/Tables/table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ReleaseVersion] (
+    ReleaseDate Datetime NOT NULL,
+    ReleaseVersion  VARCHAR (10) not NULL,
+	ReleaseDescription  VARCHAR (10) NOT NULL,
+	DBScriptFileName VARCHAR (10) NOT NULL 
+);
+
