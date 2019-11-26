2074b10634baea03d2379c833193aff4a1e429d5
Sultkon <sultkon@gmail.com>
2019-11-26 20:58:19 +0530

    column datatype changed

--- src/Schema Objects/Schemas/dbo/Tables/ReleaseVersion.table.sql
+++ src/Schema Objects/Schemas/dbo/Tables/ReleaseVersion.table.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[ReleaseVersion] (
     ReleaseDate Datetime NOT NULL,
     ReleaseVersion  VARCHAR (10) not NULL,
-	ReleaseDescription  VARCHAR (10) NOT NULL,
-	DBScriptFileName VARCHAR (10) NOT NULL 
+	ReleaseDescription  VARCHAR (100) NOT NULL,
+	DBScriptFileName VARCHAR (50) NOT NULL 
 );
 
