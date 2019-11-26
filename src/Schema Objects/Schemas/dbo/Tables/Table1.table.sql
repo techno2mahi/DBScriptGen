5b58b82cec9dc339e76bd3786eeb472b0ef0f5db
Sultkon <sultkon@gmail.com>
2019-11-26 08:26:11 +0530

    added one more table

--- src/Schema Objects/Schemas/dbo/Tables/Table1.table.sql
+++ src/Schema Objects/Schemas/dbo/Tables/Table1.table.sql
@@ -1,6 +1,41 @@
 ﻿CREATE TABLE [dbo].[Table1] (
     [Column1] NCHAR (10) NULL,
     [Column2] NCHAR (10) NULL,
-      [Column3] NCHAR (10) NULL
+    [Column3] NCHAR (10) NULL
 );
 
+
+
+
+GO
+CREATE TRIGGER dbo.trg_Table1_audit
+ON dbo.Table1
+AFTER INSERT, DELETE
+AS
+BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO [dbo].[Table1_Transaction](
+        [Column1], 
+        [Column2],
+        [Column3],
+		[CreatedDate],
+		[Action] 
+    )
+    SELECT
+         [Column1], 
+        [Column2],
+        [Column3], 
+        GETDATE(),
+        'INS'
+    FROM
+        inserted i
+    UNION ALL
+    SELECT
+      [Column1], 
+        [Column2],
+        [Column3], 
+        GETDATE(),
+        'DEL'
+    FROM
+        deleted d;
+END
---
+++ src/dbo/Tables/Table1_Transaction.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table1_Transaction] (
+    [Column1]     NCHAR (10)  NULL,
+    [Column2]     NCHAR (10)  NULL,
+    [Column3]     NCHAR (10)  NULL,
+    [CreatedDate] DATETIME    NOT NULL,
+    [Action]      VARCHAR (5) NOT NULL
+);
+
